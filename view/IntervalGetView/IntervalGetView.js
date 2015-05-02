56b2c4fe6aeb3a94bcf426302a68881d93f872e5
vomvoru <qkrcjfgus3@gmail.com>
2015-05-02 23:35:29 +0900

    alpha last version

--- view/IntervalGetView/IntervalGetView.js
+++ view/IntervalGetView/IntervalGetView.js
@@ -1,21 +1,27 @@
 define(['jquery', 'lodash', 'EventEmitter', 'tpl!/view/IntervalGetView/intervalIO.tpl'],
 	function($, _, EventEmitter, intervalIOTpl){
 
+	/* static private start */
 	var id = 0;
 	function getID(){
 		return 'interval_IO_'+id;
 	}
+	/* static private end */
 
 	function IntervalGetView(selector){
+		/* public start */
 		this.draw 				= draw;
 		this.syncInterval 		= syncInterval;
+		/* public end */
 
+		/* private start */
 		var instance = this;
 		var id = getID();
 		var interval;
 
 		var $container = $(selector);
 		var $IO;
+		/* private end */
 
 		function draw(option){
 			$container.html(intervalIOTpl({
